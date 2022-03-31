78c1cf42b11f441357a83933ac70e66fa85d654a
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2022-03-31 14:11:56 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,32 +1,22 @@
 #creates class for Node
 class Node:
- 
-def __init__(self, month=None)
-:
- 
-self.month=month
- 
-self.next=None
-#creates class for linked list 
+    def __init__(self, month=None):
+        self.month=month
+        self.next_month=None
+
+#creates class for linked list            
 class linkedList:
- 
-def __init__(self)
-:
- 
-self.head=None
- 
-def printlist(self)
-:
- 
-printed = self.head
- 
-while printed is not None:
- 
-print(printed.month)
- 
-printed = printed.next
-monthlist = linkedList()
-monthlist.head = Node("January")
+    def __init__(self):
+        self.head=None
+
+    def printlist(self):
+        printed = self.head
+        while printed is not None:
+            print(printed.month)
+            printed = printed.next
+
+month_list = linkedList()
+month_list.head = Node("January")
 n2 = Node("february")
 n3 = Node("March")
 n4 = Node("April")
@@ -38,7 +28,8 @@ n9 = Node("September")
 n10 = Node("October")
 n11 = Node("November")
 n12 = Node("December")
-monthlist.head.next = n2
+ 
+month_list.head.next = n2
 n2.next = n3
 n3.next = n4
 n4.next = n5
@@ -50,4 +41,4 @@ n9.next = n10
 n10.next = n11
 n11.next = n12
 
-monthlist.printlist()
+month_list.printlist()
